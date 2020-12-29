ecc91e6111ff0020afbbf72f4f6763d9a66a3563
Danil Ivanov <divanov@student.ethz.ch>
2020-12-29 22:49:40 +0100

    Updates

---
+++ precache-manifest.1f921e633684207c61aa9c6c01540dfe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73f84d387d44c7e0a74f5e020723ac06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b313e06bc7ed69c284b",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "f08f5c2faeba83ba75d8",
+    "url": "/static/js/2.3b189aff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.3b189aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b313e06bc7ed69c284b",
+    "url": "/static/js/main.b716670c.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "c6abb8de8f98aaa33a672683657187b0",
+    "url": "/static/media/danilivanov.c6abb8de.jpg"
+  },
+  {
+    "revision": "811ca713907a5a4178a096d3d71fc608",
+    "url": "/static/media/resume.811ca713.pdf"
+  }
+]);
